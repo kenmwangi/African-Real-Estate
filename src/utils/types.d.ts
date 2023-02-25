90d4dd43f588064b9da6aa57ff582baabcbc936d
Ken Mwangi <kenmwangi071@gmail.com>
2023-02-25 10:59:23 +0300

    fix types error

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,8 +1,8 @@
-import type { categories } from "src/constants/config";
+// import type { categories } from "src/constants/config";
 
-export type DateTime = {
-  justDate: Date | null;
-  dateTime: Date | null;
-};
+// export type DateTime = {
+//   justDate: Date | null;
+//   dateTime: Date | null;
+// };
 
-type Categories = (typeof categories)[number];
+// type Categories = (typeof categories)[number];
