d946e094c402d2c33adf2465202e897d058a6eb2
Ken Mwangi <kenmwangi071@gmail.com>
2023-02-28 19:31:25 +0300

    more cleanups

--- src/utils/types.d.ts
+++
@@ -1,8 +0,0 @@
-// import type { categories } from "src/constants/config";
-
-// export type DateTime = {
-//   justDate: Date | null;
-//   dateTime: Date | null;
-// };
-
-// type Categories = (typeof categories)[number];
