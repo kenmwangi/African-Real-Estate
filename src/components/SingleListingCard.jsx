687998fde008b5fa0859d41bf7e71b9c5d2ed0fd
Ken Mwangi <kenmwangi071@gmail.com>
2023-03-07 11:50:17 +0300

    removed login

--- src/components/SingleListingCard.jsx
+++ src/components/SingleListingCard.jsx
@@ -121,65 +121,57 @@ const SingleListingCard = ({ house }) => {
               </div> */}
             </div>
 
-            {!session ? (
-              <>
-                <div className="fixed inset-0 top-0 left-0 right-0 z-50 h-[calc(100%-1rem)] pt-10 md:h-full">
-                  <SigninPage />
-                </div>
-              </>
-            ) : (
-              <div className="flex flex-col space-y-6 md:flex-row md:items-end md:space-x-6 md:space-y-0">
-                <button
-                  onClick={(event) => setShowMobile((p) => !p)}
-                  className="flex items-center justify-center gap-4 rounded-full bg-indigo-400 px-8 py-4 font-medium text-white focus:outline-none"
-                >
-                  {showMobile ? (
-                    <p className="text-lg font-semibold lg:text-xl">
-                      {house?.telephone}
-                    </p>
-                  ) : (
-                    <>
-                      <Image
-                        src="/images/phone-icon.svg"
-                        width={15}
-                        height={15}
-                        alt="Phone Icon"
-                      />
-                      <span className="text-lg font-semibold lg:text-xl">
-                        View Phone
-                      </span>
-                    </>
-                  )}
-                </button>
-
-                <button
-                  onClick={(event) => {
-                    copyEmail(house?.email);
-                    alert(`${house.email} successfully copied to clipboard!`);
-                  }}
-                  className="flex items-center gap-4 rounded-full bg-indigo-400 px-8 py-4 font-medium text-white focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
+            <div className="flex flex-col space-y-6 md:flex-row md:items-end md:space-x-6 md:space-y-0">
+              <button
+                onClick={(event) => setShowMobile((p) => !p)}
+                className="flex items-center justify-center gap-4 rounded-full bg-indigo-400 px-8 py-4 font-medium text-white focus:outline-none"
+              >
+                {showMobile ? (
+                  <p className="text-lg font-semibold lg:text-xl">
+                    {house?.telephone}
+                  </p>
+                ) : (
+                  <>
+                    <Image
+                      src="/images/phone-icon.svg"
+                      width={15}
+                      height={15}
+                      alt="Phone Icon"
                     />
-                  </svg>
+                    <span className="text-lg font-semibold lg:text-xl">
+                      View Phone
+                    </span>
+                  </>
+                )}
+              </button>
 
-                  <span className="text-lg font-semibold lg:text-xl">
-                    Copy Email
-                  </span>
-                </button>
-              </div>
-            )}
+              <button
+                onClick={(event) => {
+                  copyEmail(house?.email);
+                  alert(`${house.email} successfully copied to clipboard!`);
+                }}
+                className="flex items-center gap-4 rounded-full bg-indigo-400 px-8 py-4 font-medium text-white focus:outline-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
+                  />
+                </svg>
+
+                <span className="text-lg font-semibold lg:text-xl">
+                  Copy Email
+                </span>
+              </button>
+            </div>
           </div>
 
           <div>
